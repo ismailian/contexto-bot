e594af3759ef327a773b48bf9d73eb76037fe0a9
ismailaatif <ismailaatif@oggvo.com>
2024-03-04 10:17:57 +0100

    handle settings updates

--- App/Handlers/Settings.php
+++ App/Handlers/Settings.php
@@ -38,6 +38,12 @@ class Settings extends BaseEvent
                     ->toArray()
             ]
         ])->sendMessage('Choose settings to customize:');
+
+        $session = SessionManager::get();
+        $session['settings']['id'] = $this->telegram->getLastMessageId();
+        unset($session['state']);
+
+        SessionManager::set($session, SessionManager::get('state'));
     }
 
     /**
@@ -45,35 +51,109 @@ class Settings extends BaseEvent
      *
      * @param IncomingCallbackQuery $query
      * @return void
+     * @throws Exception
      */
     #[CallbackQuery('settings', 'language')]
-    public function language(IncomingCallbackQuery $query): void {}
+    public function language(IncomingCallbackQuery $query): void
+    {
+        $settingsId = SessionManager::get('settings.id');
+        $lang = SessionManager::get('settings.language');
+        $isLang = fn($l) => $l == $lang ? ' ✅' : '';
+
+        if (empty($settingsId)) return;
+
+        $this->telegram->withOptions([
+            'reply_markup' => [
+                'inline_keyboard' => (new InlineKeyboard)
+                    ->setRowMax(2)
+                    ->addButton('English' . $isLang('en'), ['settings:lang' => 'en'], InlineKeyboard::CALLBACK_DATA)
+                    ->addButton('Spanish' . $isLang('es'), ['settings:lang' => 'es'], InlineKeyboard::CALLBACK_DATA)
+                    ->addButton('Portuguese' . $isLang('pt'), ['settings:lang' => 'pt'], InlineKeyboard::CALLBACK_DATA)
+                    ->toArray(),
+            ]
+        ])->editMessage($settingsId, 'Choose your language:');
+    }
 
     /**
      * handle difficulty settings
      *
      * @param IncomingCallbackQuery $query
      * @return void
+     * @throws Exception
      */
     #[CallbackQuery('settings', 'difficulty')]
-    public function difficulty(IncomingCallbackQuery $query): void {}
+    public function difficulty(IncomingCallbackQuery $query): void
+    {
+        $settingsId = SessionManager::get('settings.id');
+        $diff = SessionManager::get('settings.difficulty');
+        $isDiff = fn($d) => $d == $diff ? ' ✅' : '';
+
+        if (empty($settingsId)) return;
+
+        $this->telegram->withOptions([
+            'reply_markup' => [
+                'inline_keyboard' => (new InlineKeyboard)
+                    ->setRowMax(2)
+                    ->addButton('Easy' . $isDiff('easy'), ['settings:diff' => 'easy'], InlineKeyboard::CALLBACK_DATA)
+                    ->addButton('Medium' . $isDiff('medium'), ['settings:diff' => 'medium'], InlineKeyboard::CALLBACK_DATA)
+                    ->addButton('Hard' . $isDiff('hard'), ['settings:diff' => 'hard'], InlineKeyboard::CALLBACK_DATA)
+                    ->toArray(),
+            ]
+        ])->editMessage($settingsId, 'Choose your difficulty:');
+    }
 
     /**
      * handle language value
      *
      * @param IncomingCallbackQuery $query
      * @return void
+     * @throws Exception
      */
     #[CallbackQuery('settings:lang')]
-    public function languageChanged(IncomingCallbackQuery $query): void {}
+    public function languageChanged(IncomingCallbackQuery $query): void
+    {
+        $settingsId = SessionManager::get('settings.id');
+        $language = $query('settings:lang');
+
+        if (empty($settingsId)) return;
+        if (!in_array($language, ['en', 'es', 'pt'])) return;
+
+        $session = SessionManager::get();
+        $session['settings']['id'] = null;
+        $session['settings']['language'] = $language;
+        $session['feedback'] = $settingsId;
+
+        unset($session['state']);
+        SessionManager::set($session, SessionManager::get('state'));
+
+        $this->telegram->editMessage($settingsId, 'Your preferred language has been saved!');
+    }
 
     /**
      * handle difficulty value
      *
      * @param IncomingCallbackQuery $query
      * @return void
+     * @throws Exception
      */
     #[CallbackQuery('settings:diff')]
-    public function difficultyChanged(IncomingCallbackQuery $query): void {}
+    public function difficultyChanged(IncomingCallbackQuery $query): void
+    {
+        $settingsId = SessionManager::get('settings.id');
+        $language = $query('settings:diff');
+
+        if (empty($settingsId)) return;
+        if (!in_array($language, ['easy', 'medium', 'hard'])) return;
+
+        $session = SessionManager::get();
+        $session['settings']['id'] = null;
+        $session['settings']['difficulty'] = $language;
+        $session['feedback'] = $settingsId;
+
+        unset($session['state']);
+        SessionManager::set($session, SessionManager::get('state'));
+
+        $this->telegram->editMessage($settingsId, 'Your preferred difficulty has been saved!');
+    }
 
 }
